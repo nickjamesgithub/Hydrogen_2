f95aa66344e9dc180daeb638df54f3020232f885
Nick James <nicholas.james@sydney.edu.au>
2022-12-24 02:56:35 +1100

    update for linear operator model

--- linear_operator.py
+++ linear_operator.py
@@ -46,15 +46,13 @@ for i in range(len(fossil_year_unique)):
     fossil_capacity_list.append(fossil_capacity_vals)
 
 # Compute distance between distributions and learn linear operator
-alpha_grid = np.linspace(-10,500,30) # 30
-beta_grid = np.linspace(-5,40,30) # 20
+alpha_grid = np.linspace(-10,500,100) # 100
+beta_grid = np.linspace(-5,50,100) # 100
 
-# Store optimal values for alpha & beta
-optimal_alpha_list = []
-optimal_beta_list = []
-grid_length_list = []
-age_c_median_list = []
-age_f_median_list = []
+# Store Green optimal values for alpha & beta
+green_optimal_alpha_list = []
+green_optimal_beta_list = []
+green_grid_length_list = []
 
 for j in range(0,len(green_capacity_list)-1):
     # Print Current iteration t
@@ -93,13 +91,62 @@ for j in range(0,len(green_capacity_list)-1):
     beta_opt = beta_list[min_dist_star]
 
     # Append to optimal list
-    optimal_alpha_list.append(alpha_opt)
-    optimal_beta_list.append(beta_opt)
+    green_optimal_alpha_list.append(alpha_opt)
+    green_optimal_beta_list.append(beta_opt)
 
 # Convert alpha and beta (operator parameters) to dataframes and concatentate
-optimal_alpha_df = pd.DataFrame(optimal_alpha_list)
-optimal_beta_df = pd.DataFrame(optimal_beta_list)
-concat = pd.concat([optimal_alpha_df, optimal_beta_df], axis=1)
-concat.to_csv("/Users/tassjames/Desktop/linear_operator_parameters.csv")
+green_optimal_alpha_df = pd.DataFrame(green_optimal_alpha_list)
+green_optimal_beta_df = pd.DataFrame(green_optimal_beta_list)
+concat = pd.concat([green_optimal_alpha_df, green_optimal_beta_df], axis=1)
+concat.to_csv("/Users/tassjames/Desktop/green_linear_operator_parameters.csv")
 
-block = 1
+# Store Fossil optimal values for alpha & beta
+fossil_optimal_alpha_list = []
+fossil_optimal_beta_list = []
+fossil_grid_length_list = []
+
+for j in range(0,len(fossil_capacity_list)-1):
+    # Print Current iteration t
+    print("T_current", fossil_year_unique[j])
+    # Print Capacity at t and t+1
+    fossil_capacity_t = fossil_capacity_list[j]
+    fossil_capacity_t1 = fossil_capacity_list[j+1]
+
+    # Store values in list
+    distance_list = []
+    alpha_list = []
+    beta_list = []
+
+    for a in range(len(alpha_grid)):
+        for b in range(len(beta_grid)):
+            # Compute linear transformation
+            alpha_ = alpha_grid[a]
+            beta_ = beta_grid[b]
+            dist_transformed_capacity = alpha_ + beta_ * fossil_capacity_t
+
+            # Compute wasserstein distance
+            w_dist = wasserstein_distance(dist_transformed_capacity, fossil_capacity_t1)
+
+            # Append to lists
+            distance_list.append(w_dist)
+            alpha_list.append(alpha_)
+            beta_list.append(beta_)
+
+            print("Capacity iteration", fossil_year_unique[j])
+            print("Beta iteration", b)
+            print("Alpha iteration", a)
+
+    # Compute the minimal distance
+    min_dist_star = np.argmin(distance_list)
+    alpha_opt = alpha_list[min_dist_star]
+    beta_opt = beta_list[min_dist_star]
+
+    # Append to optimal list
+    fossil_optimal_alpha_list.append(alpha_opt)
+    fossil_optimal_beta_list.append(beta_opt)
+
+# Convert alpha and beta (operator parameters) to dataframes and concatentate
+fossil_optimal_alpha_df = pd.DataFrame(fossil_optimal_alpha_list)
+fossil_optimal_beta_df = pd.DataFrame(fossil_optimal_beta_list)
+concat = pd.concat([fossil_optimal_alpha_df, fossil_optimal_beta_df], axis=1)
+concat.to_csv("/Users/tassjames/Desktop/fossil_linear_operator_parameters.csv")
